06dea462e080a34af77078744ea16a172d6ae761
G74un3 <danielgraungaard@gmail.com>
2014-10-05 02:27:07 +0200

    Added rest of project

---
+++ postuser.php
@@ -0,0 +1,9 @@
+
+ADD RECORD.
+
+<form action="http://after-dark.dk/pvc/users" method="post">
+<input type="text" name="name" placeholder="Name" /><br>
+<input type="text" name="email" placeholder="Email" /><br>
+<input type="hidden" name="_METHOD" value="POST" />
+<input type="submit" value="A D D" />
+</form>
