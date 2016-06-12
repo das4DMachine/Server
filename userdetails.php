ebf72ae76960f6051ba78f8cc08fef012ff88399
Daniel Graungaard <danielgraungaard@gmail.com>
2016-06-12 09:41:09 +0200

    Delete userdetails.php

--- userdetails.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    const PASSWORD = "g75TD389feio";
-    const USERNAME = "graungaard_com_db_3d_towers";
-    const NAME = "graungaard_com_db_3d_towers";
-    const HOST = "graungaard.com.mysql";
-
-
-?>
